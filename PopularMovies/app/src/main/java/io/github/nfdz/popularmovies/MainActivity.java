5f0d92d6e2ff60d92236e6f712b7294ea6eb8ab8
nfdz <noe.fernandez@protonmail.com>
2017-01-23 19:45:17 +0100

    Fix project folders

---
+++ PopularMovies/app/src/main/java/io/github/nfdz/popularmovies/MainActivity.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2017 Noe Fernandez
+ */
+package io.github.nfdz.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
